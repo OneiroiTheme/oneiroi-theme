307906e5c45928bfc2585801ac44f5e47f7624ec
AEva <crystar.z@proton.me>
2025-01-21 19:36:50 +0800

    feat(nvim): 💄 modify by new palettes

--- packages/nvim/lua/oneiroi/colors/melatonin.lua
+++ packages/nvim/lua/oneiroi/colors/melatonin.lua
@@ -1,7 +1,7 @@
 ---@class oneiroi.palette
 local M = {
     fg = "#FFFDEB",
-    com = "#5B5B66",
+    com = "#3C3D4E",
     red = "#FB6BB0",
     ora = "#FF6E6B",
     yel = "#FBB66B",
@@ -9,12 +9,12 @@ local M = {
     aqu = "#6BFBB6",
     blu = "#6BB0FB",
     pur = "#B66BFB",
-    sel = "#222341",
-    cur = "#222341",
+    sel = "#121323",
+    cur = "#121323",
     bg = "#1A1B32",
 
-    fro_s = "#BCBBB5",
-    com_s = "#7B7B80",
+    fg_s = "#BAB9B3",
+    com_s = "#5F5F69",
     red_s = "#CD5790",
     ora_s = "#CD5957",
     yel_s = "#CD9457",
@@ -24,25 +24,27 @@ local M = {
     pur_s = "#9457CD",
     bac_s = "#2E3059",
 
-    bg2 = "#222341",
+    bg2 = "#121323",
     bg3 = "#0A0B14",
 
-    war = "#FBFB70",
-    war_s = "#BABA70",
-    suc = "#CAFFAA",
-    suc_s = "#8CBA76",
-    err = "#FF2525",
-    err_s = "#BA1B1B",
+    war = "#FFFA6A",
+    war_s = "#AAA746",
+    suc = "#6FFF6A",
+    suc_s = "#4AAA46",
+    err = "#FF6A6F",
+    err_s = "#AA464A",
+    inf = "#6A6FFF",
+    inf_s = "#464AAA",
 }
 M.primary = M.red
-M.secondary = M.aqu
-M.tertiary = M.yel
+M.secondary = M.yel
+M.tertiary = M.aqu
 M.quaternary = M.pur
 M.quinary = M.blu
 M.senary = M.gre
 M.primary_s = M.red_s
-M.secondary_s = M.aqu_s
-M.tertiary_s = M.yel_s
+M.secondary_s = M.yel_s
+M.tertiary_s = M.aqu_s
 M.quaternary_s = M.pur_s
 M.quinary_s = M.blu_s
 M.senary_s = M.gre_s
@@ -53,15 +55,15 @@ M.border = M.bg3
 M.muted = M.com
 M.muted_s = M.com_s
 
-M.diagInfo = M.blu_s
-M.diagHint = M.aqu_s
-M.diagWarning = M.yel_s
-M.diagSuccess = M.gre_s
-M.diagError = M.red_s
+M.diagInfo = M.inf
+M.diagHint = M.pur
+M.diagWarning = M.war
+M.diagSuccess = M.suc
+M.diagError = M.err
 
-M.diffAdd = M.gre_s
-M.diffChange = M.yel_s
-M.diffDelete = M.red_s
-M.diffText = M.blu_s
+M.diffAdd = M.gre
+M.diffChange = M.yel
+M.diffDelete = M.red
+M.diffText = M.blu
 
 return M
